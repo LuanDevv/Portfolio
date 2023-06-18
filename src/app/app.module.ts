a1907742dbaf248b543563924ce04c9b1acf7901
LuanDevv <luangcestari2006@gmail.com>
2023-06-18 17:08:56 -0300

    update 3.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
+import { TecnologiaComponent } from './tecnologia/tecnologia.component';
+import { FooterComponent } from './footer/footer.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ApresentacaoComponent,
+    TecnologiaComponent,
+    FooterComponent,
+    ExperienciasComponent
   ],
   imports: [
     BrowserModule,
